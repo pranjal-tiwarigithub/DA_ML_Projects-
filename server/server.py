2dc73b9e4ea8e392c4addc8aaa9c3eafc06b014a
pranjal-tiwarigithub <79278298+pranjal-tiwarigithub@users.noreply.github.com>
2022-05-29 22:31:13 +0530

    Add files via upload

---
+++ server/server.py
@@ -0,0 +1,57 @@
+import flask
+
+from flask import Flask , request ,jsonify
+import util
+app = Flask(__name__)
+
+
+@app.route('/get_location_names')
+
+def get_location_names():
+
+    response = jsonify({
+        'locations' : util.get_location_names()
+    })
+
+    response.headers.add('Access-contril-allow-Origin', '*')
+
+    return  response
+
+    return "hi"
+
+def hello():
+    return "hi"
+
+
+@app.route('/predict_home_price', methods = ['GET',"POST"])
+def predict_home_price():
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bhk = int(request.form['bhk'])
+    bath = int(request.form['bath'])
+
+
+    response = jsonify({
+        'estimated_price' : util.get_estimated_price(location,total_sqft,bhk,bath)
+    })
+
+    response.headers.add('Access-control-Allow-Origin','*')
+
+    return response
+
+
+if __name__ == "__main__":
+    print("starting python Flask server got Hone"
+          " for home preice prediction "
+          )
+    util.load_saved_artifacts()
+
+    app.run()
+
+
+
+
+
+
+
+
